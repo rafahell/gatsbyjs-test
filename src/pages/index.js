2bddc1d5886ded00f465ea09b114b6bb5eea13eb
Rafael <220rrs@gmail.com>
2020-04-06 10:46:13 +0100

    fix vulns

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,28 @@ import React from "react"
 import { Link } from "gatsby"
 import Container from "../components/container"
 import "../styles/global.css"
+//import axios from 'axios'
+
+class ClientFetchingExample extends Component {
+    state = {
+        loading: false,
+        error: false,
+        pupper: {
+            img: "",
+            breed: "",
+        },
+    }
+
+    componentDidMount() {
+        this.fetchRicksPupper()
+    }
+}
+
+
+const { img, breed } = this.state.pupper
 
 export default () => <div style={{ color: `purple` }}>
+
     <Container>
         <ul>
             <li><Link to="/about">About </Link></li>
@@ -17,4 +37,19 @@ export default () => <div style={{ color: `purple` }}>
         <img src="https://source.unsplash.com/random/400x200" alt="" />
     </Container>
 
+
+    {this.state.loading ? (
+        <p>Please hold, pupper incoming!</p>
+    ) : img && breed ? (
+        <>
+            <h2>{`${breed} pupper!`}</h2>
+            <img src={img} alt={`cute random `} style={{ maxWidth: 300 }} />
+        </>
+    ) : (
+                <p>Oh noes, error fetching pupper :(</p>
+            )}
 </div>
+
+
+
+
